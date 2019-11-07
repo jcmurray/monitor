40278e7fe428d479133757068133185dddcd4a86
John Murray <john@murrayfamily.eu>
2019-11-07 14:48:22 +0000

    release 0.1.2

--- constants.go
+++ constants.go
@@ -5,7 +5,7 @@ package main
 
 // Version: Version string for the app
 const (
-	ApplicationVersion     = "0.1.1"
+	ApplicationVersion     = "0.1.2"
 	DefaultHostname        = "zello.io"
 	DefaultPort            = 443
 	DefaultPath            = "/ws"
